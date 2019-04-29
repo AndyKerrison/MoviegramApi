52cce5354910629301bd582a9b4036b40edbbd51
Andrew Kerrison <adesignforlife@gmail.com>
2019-04-29 08:47:19 +0100

    Swagger integration

--- MoviegramApi/Startup.cs
+++ MoviegramApi/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace MoviegramApi
 {
@@ -26,6 +27,14 @@ namespace MoviegramApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            // Register the Swagger generator
+            // Comments are taken from the published Xml (enabled in VS build settings)
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Moviegram Api", Version = "v1" });
+                c.IncludeXmlComments(string.Format(@"{0}\MoviegramApi.XML", System.AppDomain.CurrentDomain.BaseDirectory));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,6 +49,15 @@ namespace MoviegramApi
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Moviegram Api v1");
+            });
 
             app.UseHttpsRedirection();
             app.UseMvc();
